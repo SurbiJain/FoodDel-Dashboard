d6850da6631f87ff705de33de2dd580cfd661b59
Surbhi Jain <s.jain81308@gmail.com>
2024-05-05 16:59:30 +0530

    added bgImage

--- src/scenes/logIn/LogIn.jsx
+++ src/scenes/logIn/LogIn.jsx
@@ -1,15 +1,12 @@
-import React, { useState} from "react";
-import { Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "./useAuth";
-import "./index.css"
-
+import "./index.css";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useAuth();
- 
-
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -21,37 +18,37 @@ function Login() {
     }
   };
 
- 
   return (
     <>
-      <div className="mainContent">
-        <div className="title"> Admin logIn</div>
-        <div className="register">
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+      <div className="mainContent bg">
+        
+          <div className="title"> Admin logIn</div>
+          <div className="register">
+            
+            <form onSubmit={handleLogin}>
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                  id="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type="submit">Login</button>
+            </form>
           </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        </div>
+        
       </div>
-      
-      
     </>
   );
 }
